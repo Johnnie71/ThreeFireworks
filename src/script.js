60d5ae787a4cda30b90e5c660bc1daa888be6247
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2024-08-26 13:06:09 -0400

    destroy firework with gsap

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import GUI from 'lil-gui'
 import fireworkVertexShader from './shaders/firework/vertex.glsl'
 import fireworkFragmentShader from './shaders/firework/fragment.glsl'
+import gsap from 'gsap'
 
 /**
  * Base
@@ -81,16 +82,24 @@ const textures = [
     textureLoader.load('./particles/8.png'),
 ]
 
-const createFirework = (count = 100, position, size, texture) => {
+const createFirework = (count = 100, position, size, texture, radius, color) => {
     // Geometry
     const positionsArray = new Float32Array( count * 3 )
     const sizesArray = new Float32Array(count)
 
     for (let i = 0; i < count; i++) {
         const i3 = i * 3
-        positionsArray[i3] = Math.random() - 0.5
-        positionsArray[i3 + 1] = Math.random() - 0.5
-        positionsArray[i3 + 2] = Math.random() - 0.5
+        const spherical = new THREE.Spherical(
+            radius * (0.75 + Math.random() * 0.25),
+            Math.random() * Math.PI,
+            Math.random() * Math.PI * 2
+        )
+        const position = new THREE.Vector3()
+        position.setFromSpherical(spherical)
+
+        positionsArray[i3] = position.x
+        positionsArray[i3 + 1] = position.y
+        positionsArray[i3 + 2] = position.z
 
         sizesArray[i] = Math.random()
     }
@@ -108,26 +117,55 @@ const createFirework = (count = 100, position, size, texture) => {
         uniforms: {
             uSize: new THREE.Uniform(size),
             uResolution: new THREE.Uniform(sizes.resolution),
-            uTexture: new THREE.Uniform(texture)
+            uTexture: new THREE.Uniform(texture),
+            uColor: new THREE.Uniform(color),
+            uProgess: new THREE.Uniform(0)
         },
         transparent: true,
         depthWrite: false,
         blending: THREE.AdditiveBlending
     })
 
+
     // Points
     const firework = new THREE.Points(geometry, material)
     firework.position.copy(position)
     scene.add(firework)
+
+    // Destroy
+    const destroy = () => {
+        scene.remove(firework)
+        geometry.dispose()
+        material.dispose()
+    }
+
+    // Animate
+    gsap.to(
+        material.uniforms.uProgress,
+        { value: 1, duration: 3, ease: 'linear', onComplete: destroy },
+    )
 }
 
 createFirework(
     100, // Count
     new THREE.Vector3(), // Position
     0.5, // Size
-    textures[7] // Texture
+    textures[7], // Texture,
+    1, // Radius
+    new THREE.Color('#8affff') // Color
 )
 
+window.addEventListener('click', () =>
+    {
+        createFirework(
+            100,                       // Count
+            new THREE.Vector3(),       // Position
+            0.5,                       // Size
+            textures[7],               // Texture
+            1,                         // Radius
+            new THREE.Color('#8affff') // Color
+        )
+    })
 
 /**
  * Animate
--- src/shaders/firework/vertex.glsl
+++ src/shaders/firework/vertex.glsl
@@ -1,11 +1,23 @@
 uniform float uSize;
 uniform vec2 uResolution;
 attribute float aSize;
+uniform float uProgress;
+
+float remap(float value, float originMin, float originMax, float destinationMin, float destinationMax)
+{
+    return destinationMin + (value - originMin) * (destinationMax - destinationMin) / (originMax - originMin);
+}
 
 void main()
 {
+    vec3 newPosition = position;
+
+    // Exploding
+    float explodingProgress = remap(uProgress, 0.0, 0.1, 0.0, 1.0);
+    newPosition *= explodingProgress;
+
     // Final position
-    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
     gl_Position = projectionMatrix * viewPosition;
 
